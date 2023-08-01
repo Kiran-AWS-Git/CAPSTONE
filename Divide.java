54fa8fc1075b4b55b779935f3a16b5836d506e0b
Kiran <kiran.musumalli@gmail.com>
2023-08-01 06:20:20 +0530

    4th commit from Ram_branch. Multiply and Divide files are added

---
+++ Divide.java
@@ -0,0 +1,13 @@
+import java.o.*;
+public class Divide
+{
+	public static void main(String args[])
+	{
+		int a,b,c;
+		a=50;
+		b=10;
+		c=a/b;
+		System.out.println("Quotient of "+a+" divided by "+b+" is "+c);
+	}
+}
+
